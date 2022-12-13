f324392147f52a5db2d66f02e3dc587424ddff5d
hsvrr72461 <hsvrr72461@yahoo.co.jp>
2022-12-14 07:10:58 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 新井美咲 on 2022/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
