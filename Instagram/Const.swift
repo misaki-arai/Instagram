929a71787403abf82d1316f9219d1bc0da3c7482
hsvrr72461 <hsvrr72461@yahoo.co.jp>
2022-12-25 16:33:38 +0900

    カリキュラムの範囲のみ。

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 新井美咲 on 2022/12/18.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
